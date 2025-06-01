b94efdb6c16361ca4114bccf9e84611c01e6983c
ssardarabdullah <ssardarabdulrehman58@gmail.com>
2025-06-01 16:47:09 +0500

    Update 20250601-163238.pychapter9

--- 20250601-163238.py
+++ 20250601-163238.py
@@ -1,13 +1,20 @@
-# My ticket
+Question 9______
+You can use a loop to see how
+hard it might be to win
+the kind of lottery you just 
+modeled. Make a list or tuple
+called my_ticket.
+Write a loop that keeps pulling
+numbers until your ticket wins.
+Print a messagereporting how many times the loop
+had to run to give you a winning 
+ticket.
+
+solution _________
 my_ticket = random.sample(lottery_items, 4)
-
-# Counter for attempts
 attempts = 0
-
 while True:
     new_ticket = random.sample(lottery_items, 4)
     attempts += 1
     if set(new_ticket) == set(my_ticket):
-        break
-
-print(f"Winning ticket matched after {attempts} attempts!")
+        brprint(f"Winning ticket matched after {attempts} attempts!")
