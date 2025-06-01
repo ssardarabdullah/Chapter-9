38be9c15f9017e3a2715e4576858cbc16f0291fb
ssardarabdullah <ssardarabdulrehman58@gmail.com>
2025-06-01 15:49:03 +0500

    Update 20250601-154202.pychapter9

--- 20250601-154202.py
+++ 20250601-154202.py
@@ -1,3 +1,24 @@
+Question 1___________
+
+Make a class called Restaurant.
+The init() method for
+Restaurant should store two 
+attributes: a restaurant_name 
+and a cuisine_type.
+Make a method called describe_
+restaurant() that prints these
+two pieces of
+information, and a method called
+open_restaurant() that prints a 
+message indi￾cating that the 
+restaurant is open.
+Make an instance called restaurant 
+from your class. Print the two
+attri￾butes individually, and then
+call both methods.
+
+
+solution _________
 class Restaurant:
     def __init__(self, restaurant_name, cuisine_type):
         self.restaurant_name = restaurant_name
@@ -7,16 +28,13 @@ class Restaurant:
         print(f"Restaurant Name: {self.restaurant_name}")
         print(f"Cuisine Type: {self.cuisine_type}")
 
-    def open_restaurant(self):
+ def open_restaurant(self):
         print(f"{self.restaurant_name} is now open!")
-
-# Creating an instance
+Creating an instance
 restaurant = Restaurant("Ocean Breeze", "Seafood")
-
-# Print attributes
+ Print attributes
 print(restaurant.restaurant_name)
 print(restaurant.cuisine_type)
-
-# Call methods
+ Call methods
 restaurant.describe_restaurant()
 restaurant.open_restaurant()
