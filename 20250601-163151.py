d655f530d896eb62b12fbadb03e2e6055cbbfd20
ssardarabdullah <ssardarabdulrehman58@gmail.com>
2025-06-01 16:38:01 +0500

    Update 20250601-163151.pychapter9

--- 20250601-163151.py
+++ 20250601-163151.py
@@ -1,3 +1,13 @@
+Question 7_______
+Make a class Die with one attribute
+called sides, which has a defaultvalue of 6. Write a method 
+called roll_die() that prints a
+random numberbetween 1 and the number of sides 
+the die has. Make a 6-sided die and 
+roll it10 times.
+
+
+Solution ____________
 import random
 
 class Die:
@@ -6,22 +16,16 @@ class Die:
 
     def roll_die(self):
         return random.randint(1, self.sides)
-
-# 6-sided die
 die_6 = Die()
 print("Rolling 6-sided die 10 times:")
 for _ in range(10):
     print(die_6.roll_die(), end=' ')
 print()
-
-# 10-sided die
 die_10 = Die(10)
 print("Rolling 10-sided die 10 times:")
 for _ in range(10):
     print(die_10.roll_die(), end=' ')
 print()
-
-# 20-sided die
 die_20 = Die(20)
 print("Rolling 20-sided die 10 times:")
 for _ in range(10):
