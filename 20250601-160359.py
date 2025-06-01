fdd84410846dd42ace19e36f5ddb51317011d0c5
ssardarabdullah <ssardarabdulrehman58@gmail.com>
2025-06-01 16:15:15 +0500

    Update 20250601-160359.pychapter9

--- 20250601-160359.py
+++ 20250601-160359.py
@@ -1,19 +1,24 @@
-# restaurant.py
+Question 4________
 
+ Using your latest Restaurant
+class, store it in a mod￾ule. 
+Make a separate file that imports
+Restaurant. Make a Restaurant
+instance, and call one of
+Restaurant’s methods to show that 
+the import statement is work￾ing
+properly
+
+Solution __________
 class Restaurant:
-    def __init__(self, restaurant_name, cuisine_type):
+ def __init__(self, restaurant_name, cuisine_type):
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
-
-    def describe_restaurant(self):
+ def describe_restaurant(self):
         print(f"Restaurant Name: {self.restaurant_name}")
         print(f"Cuisine Type: {self.cuisine_type}")
-
-    def open_restaurant(self):
+ def open_restaurant(self):
         print(f"{self.restaurant_name} is now open!")
-        # main_restaurant.py
-
-from restaurant import Restaurant
-
+        from restaurant import Restaurant
 my_restaurant = Restaurant("Sunset Grill", "Barbecue")
 my_restaurant.describe_restaurant()
