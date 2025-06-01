1a657b425f72e8f55b3aa5b26b5793ed184b5f36
ssardarabdullah <ssardarabdulrehman58@gmail.com>
2025-06-01 15:43:33 +0500

    Chapter 9

---
+++ 20250601-154202.py
@@ -0,0 +1,22 @@
+class Restaurant:
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+
+    def describe_restaurant(self):
+        print(f"Restaurant Name: {self.restaurant_name}")
+        print(f"Cuisine Type: {self.cuisine_type}")
+
+    def open_restaurant(self):
+        print(f"{self.restaurant_name} is now open!")
+
+# Creating an instance
+restaurant = Restaurant("Ocean Breeze", "Seafood")
+
+# Print attributes
+print(restaurant.restaurant_name)
+print(restaurant.cuisine_type)
+
+# Call methods
+restaurant.describe_restaurant()
+restaurant.open_restaurant()
