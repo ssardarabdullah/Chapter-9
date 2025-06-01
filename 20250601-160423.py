cb8daae9f9d8073ea0f0f859069ba49d85ed55da
ssardarabdullah <ssardarabdulrehman58@gmail.com>
2025-06-01 16:16:04 +0500

    Chapter9

---
+++ 20250601-160423.py
@@ -0,0 +1,30 @@
+# admin_module.py
+
+class User:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def describe_user(self):
+        print(f"User: {self.first_name} {self.last_name}")
+
+    def greet_user(self):
+        print(f"Hello, {self.first_name}!")
+
+class Privileges:
+    def __init__(self):
+        self.privileges = [
+            "can add post",
+            "can delete post",
+            "can ban user"
+        ]
+
+    def show_privileges(self):
+        print("Privileges:")
+        for privilege in self.privileges:
+            print(f"- {privilege}")
+
+class Admin(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.privileges = Privileges()
