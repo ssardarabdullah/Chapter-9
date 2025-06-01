1c35696f6d23dcadf470e6cc70d847f4529eb3d1
ssardarabdullah <ssardarabdulrehman58@gmail.com>
2025-06-01 15:52:49 +0500

    Update 20250601-154230.pychapter9

--- 20250601-154230.py
+++ 20250601-154230.py
@@ -1,9 +1,17 @@
-# Three different restaurant instances
+Question 2 ______
+
+Create threedifferent instances from the class
+, and call describe_restaurant()
+for each calle instance
+
+
+solution __________
+Three different restaurant instances
 restaurant1 = Restaurant("Spice Villa", "Indian")
 restaurant2 = Restaurant("Bella Pasta", "Italian")
 restaurant3 = Restaurant("Sushi World", "Japanese")
 
-# Call describe_restaurant() for each
+Call describe_restaurant() for each
 restaurant1.describe_restaurant()
 print()
 restaurant2.describe_restaurant()
