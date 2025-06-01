86347a040a4d135bdd8b5c43e97db919d58c99f1
ssardarabdullah <ssardarabdulrehman58@gmail.com>
2025-06-01 15:49:37 +0500

    Chapter9

---
+++ 20250601-154230.py
@@ -0,0 +1,11 @@
+# Three different restaurant instances
+restaurant1 = Restaurant("Spice Villa", "Indian")
+restaurant2 = Restaurant("Bella Pasta", "Italian")
+restaurant3 = Restaurant("Sushi World", "Japanese")
+
+# Call describe_restaurant() for each
+restaurant1.describe_restaurant()
+print()
+restaurant2.describe_restaurant()
+print()
+restaurant3.describe_restaurant()
