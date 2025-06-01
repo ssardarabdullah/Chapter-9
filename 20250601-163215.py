bb4c1a9369eff29fe5ea320e6272d84f1af886fe
ssardarabdullah <ssardarabdulrehman58@gmail.com>
2025-06-01 16:42:21 +0500

    Update 20250601-163215.pychapter9

--- 20250601-163215.py
+++ 20250601-163215.py
@@ -1,9 +1,18 @@
+Question 8______
+ You can use a loop to see how hard 
+it might be to win
+the kind of lottery you just
+modeled. Make a list or tuple 
+called my_ticket.
+Write a loop that keeps pulling 
+numbers until your ticket wins. 
+Print a messagereporting how many times the loop 
+had to run to give you a winning
+ticket.
+
+
+solution _________
 import random
-
-# Create a list of 10 numbers and 5 letters
 lottery_items = list(range(10)) + list("ABCDE")
-
-# Randomly choose 4 items
 winning_ticket = random.sample(lottery_items, 4)
-
 print(f"Any ticket matching these four numbers or letters wins a prize: {winning_ticket}")
