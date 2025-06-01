a843ecb41bde0de05646544fd276d723842ca506
ssardarabdullah <ssardarabdulrehman58@gmail.com>
2025-06-01 16:19:14 +0500

    Update 20250601-160423.pychaptet9

--- 20250601-160423.py
+++ 20250601-160423.py
@@ -1,16 +1,20 @@
-# admin_module.py
+Question 5______
+Store the classes User,
+Privileges, and Admin in one 
+module. Create a sepa￾rate file, 
+make an Admin instance, and call 
+show_privileges() to show that 
+everything is working correctly.
 
-class User:
+solution _________
+    class User:
     def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
-
-    def describe_user(self):
+  def describe_user(self):
         print(f"User: {self.first_name} {self.last_name}")
-
-    def greet_user(self):
+ def greet_user(self):
         print(f"Hello, {self.first_name}!")
-
 class Privileges:
     def __init__(self):
         self.privileges = [
@@ -18,12 +22,10 @@ class Privileges:
             "can delete post",
             "can ban user"
         ]
-
-    def show_privileges(self):
+ def show_privileges(self):
         print("Privileges:")
         for privilege in self.privileges:
             print(f"- {privilege}")
-
 class Admin(User):
     def __init__(self, first_name, last_name):
         super().__init__(first_name, last_name)
